01273c1d51b8bb21eecb91e00932f769552c8ac1
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-03-18 17:52:30 +0530

    Create 466A. Cheap Travel.cpp

---
+++ 466A. Cheap Travel.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	
+	int n,m,a,b;
+	cin>>n>>m>>a>>b;
+	int answer=0;
+ 
+ if(m*a<=b){
+ 	
+ answer=n*a;
+ 	
+ }	
+ 
+ else
+ {
+ 	answer=(n/m)*b+min((n%m)*a,b);
+ 	
+ }
+	
+	cout<<answer;
+	return 0;
+}
