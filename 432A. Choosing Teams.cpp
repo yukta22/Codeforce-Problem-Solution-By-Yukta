61d0c5e0a65aa36030e872b61713278a28df315e
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-04-02 23:10:23 +0530

    Create 432A. Choosing Teams.cpp

---
+++ 432A. Choosing Teams.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<queue>
+#define lli               long long int
+#define pb                push_back
+#define ppb               pop_back
+#define pf                push_front
+#define ppf               pop_front
+#define all(x)            (x).begin(),(x).end()
+#define uniq(v)           (v).erase(unique(all(v)),(v).end())
+#define sz(x)             (int)((x).size())
+#define fr                first
+#define sc                second
+#define pii               pair<int,int>
+#define rep(i,a,b)        for(int i=a;i<b;i++)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+const int MAX_CHAR = 26;
+using namespace std;
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("input1.txt", "r", stdin);
+    freopen("output1.txt", "w", stdout);
+#endif
+
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+
+    for(int i = 0 ; i < n ; i++){
+        cin>>a[i];
+    }
+
+    int count=0;
+
+    for(int i = 0 ; i < n ; i++){
+
+        if(5 - a[i] >= k){
+            count++;
+        }
+
+    }
+
+    int ans = count/3;
+    cout<<ans<<endl;
+
+    return 0;
+}
