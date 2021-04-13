1b5e401b4f30ac78f7b91fea1b1e8f67574b79d3
yukta22 <60733741+yukta22@users.noreply.github.com>
2021-04-13 22:04:36 +0530

    Update A. Greg's Workout.cpp

--- A. Greg's Workout.cpp
+++ A. Greg's Workout.cpp
@@ -30,15 +30,10 @@ signed main() {
     cin>>n;
 
     int a[n];
-
-    for(int i = 0 ; i < n ; i++){
-    	cin>>a[i];
-    }
-
     int ch = 0 , bi = 0 , ba =0;
 
     for(int i = 1 ; i <= n ; i++){
-    	
+    	cin>>a[i];
     	if(i % 3 == 1 ){
     		ch+= a[i];
     	}
